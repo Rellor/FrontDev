db462240b0c32e50e9021fa51ba9e16175619880
BRellor <basderoller@gmail.com>
2020-09-08 17:00:13 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+var eerstepositie = window.pageYOffset;
+
+
+window.onscroll = function() {
+  var positienu = window.pageYOffset;
+
+  if (positienu > 50) {
+    document.getElementById("header").style.background = "black";
+  } else {
+    document.getElementById("header").style.background = "linear-gradient(to bottom, rgba(18,23,28,0.75), transparent)";
+  }
+  eerstepositie = positienu;
+}
