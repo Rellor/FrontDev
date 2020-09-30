2331a2619d0249fbc27624620b839dcfe3773ca1
BRellor <basderoller@gmail.com>
2020-09-30 14:16:07 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ window.onscroll = function() {
   var positienu = window.pageYOffset;
 
   if (positienu > 50) {
-    document.getElementById("header").style.background = "black";
+    document.getElementById("header").style.background = "#12171c";
   } else {
     document.getElementById("header").style.background = "linear-gradient(to bottom, rgba(18,23,28,0.75), transparent)";
   }
